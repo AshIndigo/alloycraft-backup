8cb262d22aa14ea012f0096b58fefc2c083353d3
Ash <firedragonman958@gmail.com>
2015-02-09 18:34:31 +0100

    Test?

--- Alloycraft.java
+++ Alloycraft.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @Mod(modid = Alloycraft.MODID, version = Alloycraft.VERSION)
 public class Alloycraft
 {
-    public static final String MODID = "alloycraft2";
+    public static final String MODID = "alloycraft";
     public static final String VERSION = "1.0";
     public static Block cookieblock;
     
