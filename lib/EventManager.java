2211e228243ed3a5a3ee94eff3245f0903681e0f
Ash <firedragonman958@gmail.com>
2015-02-11 07:51:24 +0100

    Started on gui work

--- lib/EventManager.java
+++ lib/EventManager.java
@@ -2,33 +2,45 @@ package com.alloycraft.exxo.lib;
 
 import java.util.Random;
 
-import com.alloycraft.exxo.Alloycraft;
+import com.alloycraft.exxo.*;
 
-import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
-import net.minecraft.world.gen.feature.WorldGenerator;
+import cpw.mods.fml.common.IWorldGenerator;
 
 public class EventManager implements IWorldGenerator {
 
-	@Override
-public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-	    switch (world.provider.dimensionId) {
-	    case 0: //Overworld
-	    	private WorldGenerator gen_tutorial_ore; //Generates Tutorial Ore (used in Overworld)
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        switch (world.provider.dimensionId) {
+            case 0: GenerateOverworld(random, chunkX * 16, chunkZ * 16, world); break;
+            case 1: GenerateEnd(random, chunkX * 16, chunkZ * 16, world); break;
+            case -1: GenerateNether(random, chunkX * 16, chunkZ * 16, world); break;
+        }
+    }
+    public void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int minVeinSize, int maxVeinSize, int chancesToSpawn, int minY, int maxY )
+    {
+        WorldGenMinable minable = new WorldGenMinable(block, (minVeinSize + random.nextInt(maxVeinSize - minVeinSize)), Blocks.stone);
+        for(int i = 0; i < chancesToSpawn; i++)
+        {
+            int posX = blockXPos + random.nextInt(16);
+            int posY = minY + random.nextInt(maxY - minY);
+            int posZ = blockZPos + random.nextInt(16);
+            minable.generate(world, random, posX, posY, posZ);
+        }
+        }
 
-	    	public Alloycraft() {
-	    	    this.cookieblock = new WorldGenMinable(Alloycraft.tutorial_ore, 8);
-	    	}
+    private void GenerateOverworld(Random random, int x, int z, World world) {
+    	
+    	  this.addOreSpawn(Alloycraft.cookieblock, world, random, x, z, 10, 15, 5, 0, 128);
+    }
 
-	        break;
-	    case -1: //Nether
+    private void GenerateNether(Random random, int x, int z, World world) {
+    }
 
-	        break;
-	    case 1: //End
-
-	        break;
-	    }
-	}
+    private void GenerateEnd(Random random, int x, int z, World world) {
+    }
 }
--- lib/Refrences.java
+++ lib/Refrences.java
@@ -6,4 +6,6 @@ public class Refrences {
 
 	   public static final String MODID = "alloycraft";
 	   public static final String VERSION = "1.0";
+	   public static final String CLIENT_PROXY_CLASS = "com.alloycraft.exxo.lib.ClientProxy";
+	   public static final String SERVER_PROXY_CLASS = "com.alloycraft.exxo.lib.CommonProxy";
 }
